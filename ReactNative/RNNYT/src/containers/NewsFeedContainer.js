a4973df039b65b503fb4fcadf2797e4a6de860f4
BoxJeon <utrpanic@gmail.com>
2018-05-21 15:03:32 +0900

    ReactNative. RNNYT redux와 selector 적용.

--- ReactNative/RNNYT/src/containers/NewsFeedContainer.js
+++ ReactNative/RNNYT/src/containers/NewsFeedContainer.js
@@ -1,11 +1,11 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { loadNews } from '../actions/newsActions';
-// import { allNewsSelector } from '../selectors/newsSelectors';
+import { allNewsSelector } from '../selectors/newsSelectors';
 import NewsFeed from '../components/NewsFeed';
 
 const mapStateToProps = state => ({
-    news: state.news
+    news: allNewsSelector(state)
 });
 
 const mapDispatchToProps = dispatch => (
--- ReactNative/RNNYT/src/reducers/newsFeedReducer.js
+++ ReactNative/RNNYT/src/reducers/newsFeedReducer.js
@@ -3,7 +3,7 @@ import { LOAD_NEWS } from '../actions/actionTypes';
 export default (state = [], action = {}) => {
     switch (action.type) {
         case LOAD_NEWS:
-            return action.payload.result || []
+            return action.payload.results || []
         default:
             return state;
     }
