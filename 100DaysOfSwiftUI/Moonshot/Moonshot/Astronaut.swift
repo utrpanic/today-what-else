006800c7f15012cef05f26655df20a934dc2b8dd
utrpanic <utrpanic@gmail.com>
2020-04-01 00:59:22 +0900

    100DaysOfSwiftUI - Day 40
    Project 8, part 2 (Moonshot)

---
+++ 100DaysOfSwiftUI/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,7 @@
+
+struct Astronaut: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ 100DaysOfSwiftUI/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail view")) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CustomText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
