387a7f050a432a59ab5d3472c57d800fec198efb
box-jeon <utrpanic@gmail.com>
2020-09-21 19:03:02 +0900

    GitHubToy. Initial Commit.

---
+++ GitHubToy/GitHubToy/CommitsViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class CommitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ GitHubToy/GitHubToy/PullRequestsViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class PullRequestsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
