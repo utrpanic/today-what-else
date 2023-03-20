f621251c42ccc1e59566d2dd51abc218751abf08
box-jeon <utrpanic@gmail.com>
2023-03-19 22:53:45 -0400

    TikTokClone. 3.0. Initialization.

---
+++ Flutter/tiktok_clone/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tictok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xffe9435a),
+      ),
+      home: Container(),
+    );
+  }
+}
