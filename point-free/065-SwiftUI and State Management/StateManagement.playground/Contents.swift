ed0916cc71eefa4d91c1f20b0b1275420db4bbc0
box-jeon <utrpanic@gmail.com>
2023-04-01 19:29:13 -0400

    PointFree. #65 SwiftUI and State Management: Part 1.

---
+++ point-free/065-SwiftUI and State Management/StateManagement.playground/Contents.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+struct ContentView: View {
+  
+  @ObservedObject var state: AppState
+  
+  var body: some View {
+    NavigationStack {
+      List {
+        NavigationLink(destination: CounterView(state: self.state)) {
+          Text("Counter demo")
+        }
+        NavigationLink(destination: EmptyView()) {
+          Text("Favorite primes")
+        }
+      }
+      .navigationTitle("State management")
+    }
+  }
+}
+
+private func ordinal(_ n: Int) -> String {
+  let formatter = NumberFormatter()
+  formatter.numberStyle = .ordinal
+  return formatter.string(for: n) ?? ""
+}
+
+import Combine
+
+class AppState: ObservableObject {
+  @Published var count = 0
+}
+
+struct CounterView: View {
+  
+  @ObservedObject var state: AppState
+  
+  var body: some View {
+    VStack {
+      HStack {
+        Button(action: { self.state.count -= 1 }) {
+          Text("-")
+        }
+        Text("\(self.state.count)")
+        Button(action: { self.state.count += 1 }) {
+          Text("+")
+        }
+      }
+      Button(action: {}) {
+        Text("Is this prime")
+      }
+      Button(action: {}) {
+        Text("What is the \(ordinal(self.state.count)) prime")
+      }
+    }
+    .font(.title)
+    .navigationTitle("Counter demo")
+  }
+}
+
+import PlaygroundSupport
+
+PlaygroundPage.current.liveView = UIHostingController(
+  rootView: ContentView(state: AppState())
+)
