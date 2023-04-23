4b96febde6b497e17f119affa1fb0b62ecf15237
box-jeon <utrpanic@gmail.com>
2023-04-23 18:59:22 -0400

    TikTokClone. 7.9. RefreshIndicator.

--- Flutter/tiktok_clone/lib/features/videos/video_timeline_screen.dart
+++ Flutter/tiktok_clone/lib/features/videos/video_timeline_screen.dart
@@ -22,16 +22,26 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     super.dispose();
   }
 
+  Future<void> _onRefresh() {
+    return Future.delayed(const Duration(seconds: 5));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      controller: _pageController,
-      scrollDirection: Axis.vertical,
-      onPageChanged: _onPageChanged,
-      itemCount: _itemCount,
-      itemBuilder: (context, index) => VideoPost(
-        index: index,
-        onVideoFinished: _onVideoFinished,
+    return RefreshIndicator(
+      onRefresh: _onRefresh,
+      displacement: 50,
+      edgeOffset: 20,
+      color: Theme.of(context).primaryColor,
+      child: PageView.builder(
+        controller: _pageController,
+        scrollDirection: Axis.vertical,
+        onPageChanged: _onPageChanged,
+        itemCount: _itemCount,
+        itemBuilder: (context, index) => VideoPost(
+          index: index,
+          onVideoFinished: _onVideoFinished,
+        ),
       ),
     );
   }
