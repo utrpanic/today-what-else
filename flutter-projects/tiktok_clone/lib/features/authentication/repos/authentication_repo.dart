953bcc36995340feab6c4de164855052dcb3475b
box-jeon <utrpanic@gmail.com>
2024-01-16 13:33:45 -0500

    TikTokClone. 24.1 signOut.

--- flutter-projects/tiktok_clone/lib/features/authentication/repos/authentication_repo.dart
+++ flutter-projects/tiktok_clone/lib/features/authentication/repos/authentication_repo.dart
@@ -7,6 +7,8 @@ class AuthenticationRepository {
   bool get isLoggedIn => user != null;
   User? get user => _firebaseAuth.currentUser;
 
+  Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
+
   Future<void> signUp(String? email, String? password) async {
     if (email == null || password == null) {
       throw Exception('Email and password must not be null');
@@ -16,6 +18,15 @@ class AuthenticationRepository {
       password: password,
     );
   }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
 }
 
 final authRepo = Provider((ref) => AuthenticationRepository());
+
+final authState = StreamProvider((ref) {
+  final repo = ref.watch(authRepo);
+  return repo.authStateChanges();
+});
--- flutter-projects/tiktok_clone/lib/features/settings/settings_screen.dart
+++ flutter-projects/tiktok_clone/lib/features/settings/settings_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone/features/videos/models/view_models/playback_config_view_model.dart';
 
 class SettingsScreen extends ConsumerWidget {
@@ -141,7 +143,10 @@ class SettingsScreen extends ConsumerWidget {
                     ),
                     CupertinoActionSheetAction(
                       isDestructiveAction: true,
-                      onPressed: Navigator.of(context).pop,
+                      onPressed: () {
+                        ref.read(authRepo).signOut();
+                        context.go('/');
+                      },
                       child: const Text('Yes, plz'),
                     ),
                   ],
