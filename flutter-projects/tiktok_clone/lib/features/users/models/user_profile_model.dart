18a58de7fee86e46b3c6bf61f94357efeaa6dbbe
box-jeon <utrpanic@gmail.com>
2024-01-17 11:47:40 -0500

    TikTokClone. 25.4 findProfile.

--- flutter-projects/tiktok_clone/lib/features/users/models/user_profile_model.dart
+++ flutter-projects/tiktok_clone/lib/features/users/models/user_profile_model.dart
@@ -16,6 +16,15 @@ class UserProfileModel {
           link: '',
         );
 
+  UserProfileModel.fromJson(Map<String, dynamic> json)
+      : this(
+          uid: json['uid'] as String,
+          email: json['email'] as String,
+          name: json['name'] as String,
+          bio: json['bio'] as String,
+          link: json['link'] as String,
+        );
+
   final String uid;
   final String email;
   final String name;
--- flutter-projects/tiktok_clone/lib/features/users/view_models/users_view_model.dart
+++ flutter-projects/tiktok_clone/lib/features/users/view_models/users_view_model.dart
@@ -2,15 +2,30 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone/features/users/models/user_profile_model.dart';
 import 'package:tiktok_clone/features/users/repos/user_repo.dart';
 
 class UsersViewModel extends AsyncNotifier<UserProfileModel> {
-  late final UserRepository _repository;
+  late final UserRepository _userRepository;
+  late final AuthenticationRepository _authenticationRepository;
+
   @override
-  FutureOr<UserProfileModel> build() {
-    _repository = ref.read(userRepo);
-    return UserProfileModel.empty();
+  FutureOr<UserProfileModel> build() async {
+    _userRepository = ref.read(userRepo);
+    _authenticationRepository = ref.read(authRepo);
+    if (_authenticationRepository.isLoggedIn) {
+      final profile = await _userRepository.findProfile(
+        _authenticationRepository.user!.uid,
+      );
+      if (profile != null) {
+        return UserProfileModel.fromJson(profile);
+      } else {
+        return UserProfileModel.empty();
+      }
+    } else {
+      return UserProfileModel.empty();
+    }
   }
 
   Future<void> createProfile(UserCredential credential) async {
@@ -26,7 +41,7 @@ class UsersViewModel extends AsyncNotifier<UserProfileModel> {
       name: credential.user!.displayName ?? 'Anon',
     );
     state = AsyncValue.data(profile);
-    await _repository.createProfile(profile);
+    await _userRepository.createProfile(profile);
   }
 }
 
