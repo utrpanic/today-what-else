78d040072554bf009855ec556b2e105a76ec7708
box-jeon <utrpanic@gmail.com>
2024-01-18 10:28:29 -0500

    TikTokClone. 26.4 ffmpeg.

--- flutter-projects/tiktok_clone/functions/src/index.ts
+++ flutter-projects/tiktok_clone/functions/src/index.ts
@@ -1,10 +1,28 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+
 admin.initializeApp();
 
 export const onVideoCreated = functions.firestore
     .document('videos/{videoId}')
     .onCreate(async (snapshot, context) => {
-        await snapshot.ref.update({'hello': 'from functions'})
+        // https://www.npmjs.com/package/child-process-promise
+        const spawn = require('child-process-promise').spawn;
+        const video = snapshot.data();
+        await spawn('ffmpeg', [
+            '-i',
+            video.fileUrl,
+            '-ss',
+            '00:00:01.000',
+            '-vframes',
+            '1',
+            '-vf',
+            'scale=150:-1',
+            `/tmp/${snapshot.id}.jpg`,
+        ]);
+        const storage = admin.storage();
+        await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+            destination: `thumbnails/${snapshot.id}.jpg`,
+        });
     });
