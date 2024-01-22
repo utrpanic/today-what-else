d55c2dc38d8e1999f09db6b1e000a1daaccde45b
box-jeon <utrpanic@gmail.com>
2024-01-21 21:34:40 -0500

    TikTokClone. 28.2 StreamProvider.

--- flutter-projects/tiktok_clone/lib/features/inbox/models/message_model.dart
+++ flutter-projects/tiktok_clone/lib/features/inbox/models/message_model.dart
@@ -2,21 +2,25 @@ class MessageModel {
   MessageModel({
     required this.text,
     required this.userId,
+    required this.createdAt,
   });
 
   MessageModel.fromJson(Map<String, dynamic> json)
       : this(
           text: json['text'] as String,
           userId: json['userId'] as String,
+          createdAt: json['createdAt'] as int,
         );
 
   Map<String, dynamic> toJson() {
     return {
       'text': text,
       'userId': userId,
+      'createdAt': createdAt,
     };
   }
 
   final String text;
   final String userId;
+  final int createdAt;
 }
--- flutter-projects/tiktok_clone/lib/features/inbox/view_models/messages_view_model.dart
+++ flutter-projects/tiktok_clone/lib/features/inbox/view_models/messages_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone/features/inbox/models/message_model.dart';
@@ -17,7 +18,11 @@ class MessagesViewModel extends AsyncNotifier<void> {
     final user = ref.read(authRepo).user;
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
-      final message = MessageModel(text: text, userId: user!.uid);
+      final message = MessageModel(
+        text: text,
+        userId: user!.uid,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+      );
       await _repository.sendMessage(message);
     });
   }
@@ -25,4 +30,23 @@ class MessagesViewModel extends AsyncNotifier<void> {
 
 final messagesProvider = AsyncNotifierProvider<MessagesViewModel, void>(
   MessagesViewModel.new,
+);
+
+final chatProvider = StreamProvider<List<MessageModel>>(
+  (ref) {
+    final db = FirebaseFirestore.instance;
+    return db
+        .collection('chat_rooms')
+        .doc('fSaCvpMQ3qTcWeHssrTT')
+        .collection('texts')
+        .orderBy('createdAt')
+        .snapshots()
+        .map(
+          (event) => event.docs
+              .map(
+                (doc) => MessageModel.fromJson(doc.data()),
+              )
+              .toList(),
+        );
+  },
 );
