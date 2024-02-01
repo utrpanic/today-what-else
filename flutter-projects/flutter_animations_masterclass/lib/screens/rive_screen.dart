94b9650f5f8f928f1a163c4305f72cde5fcd1d16
box-jeon <utrpanic@gmail.com>
2024-02-01 12:59:36 -0500

    FlutterAnimations. 6.4 Custom Animation, 6.5 Animation Blur.

--- flutter-projects/flutter_animations_masterclass/lib/screens/rive_screen.dart
+++ flutter-projects/flutter_animations_masterclass/lib/screens/rive_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -9,42 +11,27 @@ class RiveScreen extends StatefulWidget {
 }
 
 class _RiveScreenState extends State<RiveScreen> {
-  late final StateMachineController _stateMachineController;
-
-  void _onInit(Artboard artboard) {
-    _stateMachineController = StateMachineController.fromArtboard(
-      artboard,
-      'state',
-      onStateChange: (stateMachineName, stateName) {
-        debugPrint('State Machine: $stateMachineName, State: $stateName');
-      },
-    )!;
-    artboard.addController(_stateMachineController);
-  }
-
-  @override
-  void dispose() {
-    _stateMachineController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Rive'),
-      ),
-      body: Center(
-        child: Container(
-          color: const Color(0xFFFF2ECC),
-          width: double.infinity,
-          child: RiveAnimation.asset(
-            'assets/animations/stars-animation.riv',
-            artboard: 'artboard',
-            onInit: _onInit,
-            stateMachines: const ['state'],
+      body: Stack(
+        children: [
+          const RiveAnimation.asset(
+            'assets/animations/balls-animation.riv',
+            fit: BoxFit.cover,
           ),
-        ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: const Center(
+                child: Text(
+                  'Welcome to AI app',
+                  style: TextStyle(fontSize: 32),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
