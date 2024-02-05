49e80694f8837ded7b5e697152e59f3beff89a3e
box-jeon <utrpanic@gmail.com>
2024-02-05 12:28:25 -0500

    FlutterAnimations. 8.3 Credit Cards.

--- flutter-projects/flutter_animations_masterclass/lib/screens/wallet_screen.dart
+++ flutter-projects/flutter_animations_masterclass/lib/screens/wallet_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 
 class WalletScreen extends StatefulWidget {
   const WalletScreen({super.key});
@@ -15,28 +14,94 @@ class _WalletScreenState extends State<WalletScreen> {
       appBar: AppBar(
         title: const Text('Wallet'),
       ),
-      body: Center(
-        child: const Text(
-          'Hello!',
-          style: TextStyle(fontSize: 64),
-        )
-            .animate()
-            .fadeIn(
-              begin: 0,
-              duration: 2000.ms,
-            )
-            .scale(
-              alignment: Alignment.center,
-              begin: Offset.zero,
-              end: const Offset(1, 1),
-              duration: 2000.ms,
-            )
-            .then(delay: 2000.ms)
-            .slideX(
-              begin: 0,
-              end: -10,
-              duration: 2000.ms,
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            CreditCard(bgColor: Colors.purple),
+            CreditCard(bgColor: Colors.black),
+            CreditCard(bgColor: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CreditCard extends StatelessWidget {
+  const CreditCard({super.key, required this.bgColor});
+
+  final Color bgColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(bottom: 32),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: bgColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32,
+          vertical: 40,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 100),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Nomad Coders',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      '**** **** **** **75',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Positioned(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 20,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
+          ],
+        ),
       ),
     );
   }
